e5680c6ee1f552c540f1573b69f536310b6d91d2
Eunbin Kang <kub0803@naver.com>
2023-01-14 17:22:05 +0900

    chore

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import { BrowserRouter, useRoutes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+import NotFound from "./page/NotFound";
+import routes from "./routes";
+import Map from "./page/main/components/Map";
+
+function App() {
+  const routing = useRoutes(routes);
+
+  return (
+    <>
+      <BrowserRouter>
+        <QueryClientProvider client={new QueryClient()}>
+          {routing}
+          <Map />
+        </QueryClientProvider>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
