4b2828409e8f80e54b6e0aaa9dbf2b177b392666
username <81845513+june3011@users.noreply.github.com>
2023-01-14 23:30:58 +0900

    feat : 마커 호버 시 나타나는 툴팁 (친구 상태 창)

--- src/page/main/components/Map.tsx
+++ src/page/main/components/Map.tsx
@@ -1,10 +1,13 @@
 import React, { useEffect, useRef, useState } from "react";
 import styled from "@emotion/styled";
 import MarkerHtml from "./marker";
+import ArrowTooltip from "../../../components/ArrowTooltip";
+import { Tooltip } from "@mui/material";
 
 const Map = () => {
   const mapRef = useRef<HTMLElement | null | any>(null);
-  const markerRef = useRef<any | null>(null);
+
+  const [markerHover, setMarkerHover] = useState<boolean>(false);
 
   const [myLocation, setMyLocation] = useState<
     | {
@@ -56,44 +59,116 @@ const Map = () => {
   }, [myLocation]);
 
   useEffect(() => {
-    comunities.map((item: any) => {
+    const handleMarker = (item: any) => {
       new naver.maps.Marker({
         position: new naver.maps.LatLng(item.latitude, item.longitude),
         map: mapRef.current,
+
         icon: {
           content: MarkerHtml(
-            "https://item.kakaocdn.net/do/30cef086c8778d80e1487385bd5efe7b8f324a0b9c48f77dbce3a43bd11ce785"
+            markerHover,
+            "https://item.kakaocdn.net/do/30cef086c8778d80e1487385bd5efe7b8f324a0b9c48f77dbce3a43bd11ce785",
+            "다들 어디?! 오늘 나랑 수다 떨어줄 사람 심심해요 ㅠㅠ ㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜ"
           ),
           size: new naver.maps.Size(38, 58),
           anchor: new naver.maps.Point(19, 58),
         },
       });
+    };
+
+    const handleHoverState = () => {
+      setMarkerHover((prev) => !prev);
+    };
+
+    comunities.map((item: any) => {
+      return (
+        <Tooltip
+          disableFocusListener
+          open={markerHover}
+          onClose={handleHoverState}
+          onOpen={handleHoverState}
+          title="asdfasdf"
+          placement="left-start"
+        >
+          <>{handleMarker(item)}</>
+        </Tooltip>
+      );
     });
   }, [comunities]);
 
-  return <SContainer ref={mapRef} id="map"></SContainer>;
+  return (
+    <SContainer markerState={markerHover} ref={mapRef} id="map"></SContainer>
+  );
 };
 
 export default Map;
 
-const SContainer = styled.div`
+const SContainer = styled.div<{ markerState: boolean }>`
   width: 100%;
   height: 100vh;
 
   #marker-wrap {
     display: flex;
     align-items: center;
-    width: 80px;
+    width: auto;
     height: 80px;
     position: relative;
+
+    &:hover {
+      #tooltip {
+        opacity: 1;
+      }
+    }
+
+    #tooltip {
+      opacity: 0;
+      width: 275px;
+      height: 430px;
+      background-color: #ffffff;
+      border-radius: 20px;
+      z-index: 3;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      align-items: center;
+      /* gap: 10px; */
+      padding-bottom: 10px;
+    }
   }
 
   #img-circle {
     position: absolute;
-    left: 50%;
+    left: 11.3%;
+    bottom: 30%;
     transform: translate(-50%, -10%);
     width: 40px;
     height: 40px;
     border-radius: 50px;
+
+    &:hover {
+      background-color: blue;
+    }
+  }
+
+  #tooltip-profile-img {
+    display: flex;
+    width: 100%;
+    height: 300px;
+    background-color: white;
+    border-radius: 20px 20px 0 0;
+    background-color: black;
+  }
+
+  #tooltip-content {
+    display: flex;
+    height: 35px;
+    font-size: 13px;
+    align-items: center;
+    padding: 5px 10px;
+  }
+
+  #messege-start {
+    width: 60px;
+    height: 60px;
   }
 `;
--- src/page/main/components/marker.tsx
+++ src/page/main/components/marker.tsx
@@ -1,7 +1,15 @@
-const MarkerHtml = (url: string) => `
+const MarkerHtml = (hoverState: boolean, url: string, content: string) => `   
     <div id="marker-wrap">
-        <img src="/assets/marker-bg.svg">
+    <div>
+      <img src="/assets/marker-bg.svg">
         <img id="img-circle" src=${url}>
+    </div>
+    
+        <div id="tooltip">
+            <img id="tooltip-profile-img" src=${url}>
+            <div id="tooltip-content">${content}</div>
+            <img id="messege-start" src="/assets/chat_btn.svg">
+        </div>
     </div>
 `;
 
